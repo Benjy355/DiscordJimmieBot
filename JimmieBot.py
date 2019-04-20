ed5a1db6fb9dbbff41a7b9935a1624e605fe16e2
Benjy355 <benjy355@gmail.com>
2019-04-20 16:02:53 -0700

    Started getting the initialization process going

--- JimmieBot.py
+++ JimmieBot.py
@@ -8,6 +8,22 @@ import asyncio
 import discord
 
 class JimmieBot(discord.Client):
+    COMMANDS = {}
+    #RegEx pattern
+    PATTERNS = []
+
+    COMMAND_PREFIX = "!"
+
     async def on_ready(self):
         print("Connected to Discord")
         print("%s (%s)\n" % (self.user.name, self.user.id))
+    
+    async def on_message(self, message: discord.message.Message):
+        if (message.author == self.user):
+            return
+        if (not message.content.startswith(self.COMMAND_PREFIX)):
+            return
+            
+        cleaned_str = message.content.split(" ", 1)
+        #[n:] removes the prefix
+        cleaned_str = cleaned_str[0][len(self.COMMAND_PREFIX):].lower()
