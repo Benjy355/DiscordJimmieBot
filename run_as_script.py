5a2e9619256c8a7cee289ec8df9075907328ff45
Benjy355 <benjy355@gmail.com>
2019-05-04 18:36:11 -0700

    Deleted test modules
    TODO: Make example module, once the structure is complete

--- run_as_script.py
+++ run_as_script.py
@@ -5,3 +5,4 @@ from discord_oauth import BOT_TOKEN
 jBot = JimmieBot()
 jBot.run(BOT_TOKEN)
 #Should we try to catch it when it crashes? (As it always does)
+#No. We shouldn't, until we fix our module loading system to not double load modules on startup
